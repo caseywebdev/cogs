e2f92a95aa5807d162ba3acde7fc3fc4a3196749
Casey Foster <c@sey.me>
2013-03-17 16:49:25 -0500

    Use async library to clean up some hairy code

--- test/test.js
+++ test/test.js
@@ -1,4 +1,5 @@
 var _ = require('underscore');
+var async = require('async');
 var fs = require('fs');
 require('chai').should();
 var xl8 = require('..');
@@ -25,27 +26,21 @@ describe('Expected/Actual Comparisons', function () {
   _.each(dirs, function (dir) {
     if (dir[0] === '.') return;
     it(dir, function (done) {
-      var expected;
-      var result;
-      var compare = _.after(2, function () {
-        result.should.equal(expected);
+      async.map([
+        {file: '/a', method: 'build', property: 'built'},
+        {file: '/expected', method: 'update', property: 'raw'}
+      ], function (obj, cb) {
+        xl8.asset(dir + obj.file, function (er, asset) {
+          if (er) return cb(er);
+          asset[obj.method](function (er) {
+            if (er) return cb(er);
+            cb(null, asset[obj.property].trim().replace(/\n\s*/g, '\n'));
+          });
+        });
+      }, function (er, result) {
+        if (er) return done(er);
+        result[0].should.equal(result[1]);
         done();
-      });
-      xl8.asset(dir + '/expected', function (er, asset) {
-        if (er) return done(er);
-        asset.update(function (er) {
-          if (er) return done(er);
-          expected = asset.raw.trim().replace(/\n\s*/g, '\n');
-          compare();
-        });
-      });
-      xl8.asset(dir + '/a', function (er, asset) {
-        if (er) return done(er);
-        asset.build(function (er) {
-          if (er) return done(er);
-          result = asset.built.trim().replace(/\n\s*/g, '\n');
-          compare();
-        });
       });
     });
   });
