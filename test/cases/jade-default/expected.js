5e62be2a79c8ba6c3b0f0415758026432ff7d19a
Casey Foster <c@sey.me>
2013-04-19 15:16:27 -0500

    Add ignore option, fix jade tests, better errors, 0.8.0

--- test/cases/jade-default/expected.js
+++ test/cases/jade-default/expected.js
@@ -1,10 +1,8 @@
 // test/cases/jade-default/a.jade
-(window.jst || (window.jst = {}))['jade-default/a'] = function anonymous(locals, attrs, escape, rethrow, merge) {
-attrs = attrs || jade.attrs; escape = escape || jade.escape; rethrow = rethrow || jade.rethrow; merge = merge || jade.merge;
+(window.jst || (window.jst = {}))['jade-default/a'] = function anonymous(locals) {
 var buf = [];
 with (locals || {}) {
-var interp;
-buf.push('<!DOCTYPE html><html><head></head><body></body></html>');
+buf.push("<!DOCTYPE html><html><head></head><body></body></html>");
 }
 return buf.join("");
 };
