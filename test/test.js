020c4064a40c6efe95a917d02653aaab2d3a2410
Casey Foster <c@sey.me>
2013-04-25 16:03:37 -0500

    Add preliminary fingerprinting support, 0.9.11

--- test/test.js
+++ test/test.js
@@ -1,23 +1,54 @@
 var _ = require('underscore');
 var async = require('async');
+var crypto = require('crypto');
 var fs = require('fs');
-require('chai').should();
+var expect = require('chai').expect;
 var cogs = require('..');
 
 var describe = global.describe;
 var it = global.it;
+var before = global.before;
 
 describe('Env Setup', function () {
-  it('should add new paths', function () {
+  it('adds new paths', function () {
     cogs.addPaths('test/cases');
   });
 
-  it('should get the base of a filename with dots', function () {
-    cogs.split('/a/b.html.jade.html.jade').base.should.equal('b.html.jade');
+  it('gets the base of a filename with dots', function () {
+    expect(cogs.split('/a/b.html.jade.html.jade').base).to.equal('b.html.jade');
   });
 
-  it('should get the extensions of a filename with dots', function () {
-    cogs.split('/a/b.html.coffee.html.jade').exts.should.eql(['html', 'jade']);
+  it('gets the extensions of a filename with dots', function () {
+    expect(cogs.split('/a/b.html.coffee.html.jade').exts)
+      .to.eql(['html', 'jade']);
+  });
+});
+
+describe('Asset', function () {
+  before(function (done) {
+    var self = this;
+    cogs.asset('coffee/a', function (er, asset) {
+      if (er) return done(er);
+      self.asset = asset;
+      asset.build(function (er) {
+        if (er) return done(er);
+        var str = asset.toString();
+        self.md5 = crypto.createHash('md5').update(str).digest('hex');
+        self.sha1 = crypto.createHash('sha1').update(str).digest('hex');
+        done();
+      });
+    });
+  });
+
+  it('can return a hex fingerprint', function () {
+    expect(_.bind(this.asset.fingerprint, this.asset)).to.throw(Error);
+    expect(this.asset.fingerprint('md5')).to.equal(this.md5);
+    expect(this.asset.fingerprint('sha1')).to.equal(this.sha1);
+  });
+
+  it('can generate a fingerprinted filename', function () {
+    expect(this.asset.filename('md5')).to.equal('a-' + this.md5 + '.js');
+    expect(this.asset.filename('sha1')).to.equal('a-' + this.sha1 + '.js');
   });
 });
 
@@ -39,7 +70,7 @@ describe('Expected/Actual Comparisons', function () {
         });
       }, function (er, result) {
         if (er) return done(er);
-        result[0].should.equal(result[1]);
+        expect(result[0]).to.equal(result[1]);
         done();
       });
     });
