5cd9521b8524f116b079c51282a2b19e053994ca
Casey Foster <c@sey.me>
2013-07-15 16:36:00 -0500

    Update jade tests, 0.10.12

--- test/cases/jade-jst/expected.js
+++ test/cases/jade-jst/expected.js
@@ -1,8 +1,5 @@
 // test/cases/jade-jst/a.jst.jade
 (window.jst || (window.jst = {}))['jade-jst/a'] = function anonymous(locals) {
-var buf = [];
-with (locals || {}) {
-buf.push("<!DOCTYPE html><html><head></head><body></body></html>");
-}
-return buf.join("");
+  var buf = [];
+  var locals_ = (locals || {}),title = locals_.title,body = locals_.body;buf.push("<!DOCTYPE html><html><head><title>" + (jade.escape(null == (jade.interp = title) ? "" : jade.interp)) + "</title></head><body><body>" + (jade.escape(null == (jade.interp = body) ? "" : jade.interp)) + "</body></body></html>");;return buf.join("");
 };
