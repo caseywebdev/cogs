4799188d2da78eded873a40f02b76be0d4f6b6ca
Casey Foster <c@sey.me>
2013-12-04 23:16:26 -0600

    I have no idea what I'm doing, 0.13.5

--- test/cases/mustache-default/expected.js
+++ test/cases/mustache-default/expected.js
@@ -1,2 +1,2 @@
 // test/cases/mustache-default/a.mustache
-(window.jst || (window.jst = {}))['mustache-default/a'] = function (data) { return Mustache.render([["text","<h1>hello ",0,10],["&","name",10,20],["text","</h1>\n",20,26]], data); };
+(window.jst || (window.jst = {}))['mustache-default/a'] = function (data) { return Mustache.render("<h1>hello {{{name}}}</h1>\n", data); };
