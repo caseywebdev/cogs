421775383a1996caed338da70e5649e46dee40eb
Casey Foster <c@sey.me>
2013-12-04 19:05:22 -0600

    Add mustache support, 0.13.3

---
+++ test/cases/mustache-default/expected.js
@@ -0,0 +1,2 @@
+// test/cases/mustache-default/a.mustache
+(window.jst || (window.jst = {}))['mustache-default/a'] = function (data) { return Handlebars.render([["text","<h1>hello ",0,10],["&","name",10,20],["text","</h1>\n",20,26]], data); };
---
+++ test/cases/mustache-jst/expected.js
@@ -0,0 +1,2 @@
+// test/cases/mustache-jst/a.jst.mustache
+(window.jst || (window.jst = {}))['mustache-jst/a'] = function (data) { return Handlebars.render([["text","<h1>hello ",0,10],["&","name",10,20],["text","</h1>\n",20,26]], data); };
