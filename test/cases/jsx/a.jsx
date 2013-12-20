e3bd8676544f1d11cbabcf61f0cbd5e7c9acb81c
Casey Foster <c@sey.me>
2013-12-20 11:09:25 -0600

    Add support for Facebook JSX syntax, 0.15.9

---
+++ test/cases/jsx/a.jsx
@@ -0,0 +1,9 @@
+/** @jsx React.DOM */
+
+var HelloMessage = React.createClass({
+  render: function () {
+    return <div>{'Hello ' + this.props.name}</div>;
+  }
+});
+
+React.renderComponent(<HelloMessage name='John' />, mountNode);
