230c068f6f645fe0315971bed05b31440f95dc90
Casey Foster <c@sey.me>
2014-01-02 15:58:08 -0600

    Update jade tests/implementation, 0.16.2

--- test/cases/jade-jst/expected.js
+++ test/cases/jade-jst/expected.js
@@ -1,15 +1,17 @@
 // test/cases/jade-jst/a.jst.jade
 (function (root, factory) {
-  if (typeof define === 'function' && define.amd) {
-    define('jade-jst/a', ['jade', 'mustache', 'underscore'], factory);
-  }
-  if (typeof exports !== 'undefined') {
-    module.exports = factory(require('jade'), require('mustache'), require('underscore'));
-  }
-  (root.JST || (root.JST = {}))['jade-jst/a'] = factory(root['jade'], root['Mustache'], root['_']);
+if (typeof define === 'function' && define.amd) {
+define('jade-jst/a', ['jade', 'mustache', 'underscore'], factory);
+}
+if (typeof exports !== 'undefined') {
+module.exports = factory(require('jade'), require('mustache'), require('underscore'));
+}
+(root.JST || (root.JST = {}))['jade-jst/a'] = factory(root['jade'], root['Mustache'], root['_']);
 })(this, function (jade, Mustache, _) {
-  return (function anonymous(locals) {
+return (function template(locals) {
 var buf = [];
-var locals_ = (locals || {}),title = locals_.title,body = locals_.body;buf.push("<!DOCTYPE html><html><head><title>" + (jade.escape(null == (jade.interp = title) ? "" : jade.interp)) + "</title></head><body><body>" + (jade.escape(null == (jade.interp = body) ? "" : jade.interp)) + "</body></body></html>");;return buf.join("");
+var jade_mixins = {};
+var locals_ = (locals || {}),title = locals_.title,body = locals_.body;
+buf.push("<!DOCTYPE html><html><head><title>" + (jade.escape(null == (jade.interp = title) ? "" : jade.interp)) + "</title></head><body><body>" + (jade.escape(null == (jade.interp = body) ? "" : jade.interp)) + "</body></body></html>");;return buf.join("");
 });
 });
