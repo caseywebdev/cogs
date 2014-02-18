f26e28428ac77fb5260a697284a7faec233c01c2
Casey Foster <c@sey.me>
2014-02-18 10:32:53 -0600

    Add JSON engine, 0.19.0

---
+++ test/cases/json/expected.js
@@ -0,0 +1,15 @@
+// test/cases/json/a.json
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define([], factory);
+  } else if (typeof exports !== 'undefined') {
+    module.exports = factory();
+  } else {
+    root['json/a'] = factory();
+  }
+})(this, function () {
+return {
+  "this": "is",
+  "some": ["json"]
+}
+});
