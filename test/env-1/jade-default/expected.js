671f15082b7761447ccf4731f719ce68d10a2b74
Casey Foster <c@sey.me>
2014-02-27 00:58:12 -0600

    Update tests for new jade output, 0.21.2

--- test/env-1/jade-default/expected.js
+++ test/env-1/jade-default/expected.js
@@ -1,16 +1,17 @@
 // test/env-1/jade-default/a.jade
 (function (root, factory) {
-  if (typeof define === 'function' && define.amd) {
-    define('jade-default/a', ['jade', 'mustache', 'underscore'], factory);
-  } else if (typeof exports !== 'undefined') {
-    module.exports = factory(require('jade'), require('mustache'), require('underscore'));
-  } else {
-    root['jade-default/a'] = factory(root['jade'], root['Mustache'], root['_']);
-  }
+if (typeof define === 'function' && define.amd) {
+define('jade-default/a', ['jade', 'mustache', 'underscore'], factory);
+} else if (typeof exports !== 'undefined') {
+module.exports = factory(require('jade'), require('mustache'), require('underscore'));
+} else {
+root['jade-default/a'] = factory(root['jade'], root['Mustache'], root['_']);
+}
 })(this, function (jade, Mustache, _) {
 return function template(locals) {
 var buf = [];
 var jade_mixins = {};
+var jade_interp;
 var locals_ = (locals || {}),title = locals_.title,body = locals_.body;
-buf.push("<!DOCTYPE html><html><head><title>" + (jade.escape(null == (jade.interp = title) ? "" : jade.interp)) + "</title></head><body><body>" + (jade.escape(null == (jade.interp = body) ? "" : jade.interp)) + "</body></body></html>");;return buf.join("");
+buf.push("<!DOCTYPE html><html><head><title>" + (jade.escape(null == (jade_interp = title) ? "" : jade_interp)) + "</title></head><body><body>" + (jade.escape(null == (jade_interp = body) ? "" : jade_interp)) + "</body></body></html>");;return buf.join("");
 }});
