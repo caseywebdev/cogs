663cfa711c71c74232831b4f590c2327de2e2a1b
Casey Foster <c@sey.me>
2014-08-25 16:43:52 -0500

    Fix jade tests

--- test/env-1/jade-jst/expected.js
+++ test/env-1/jade-jst/expected.js
@@ -13,5 +13,5 @@ var buf = [];
 var jade_mixins = {};
 var jade_interp;
 ;var locals_for_with = (locals || {});(function (title, body) {
-buf.push("<!DOCTYPE html><html><head><title>" + (jade.escape(null == (jade_interp = title) ? "" : jade_interp)) + "</title></head><body><body>" + (jade.escape(null == (jade_interp = body) ? "" : jade_interp)) + "</body></body></html>");}("title" in locals_for_with?locals_for_with.title:typeof title!=="undefined"?title:undefined,"body" in locals_for_with?locals_for_with.body:typeof body!=="undefined"?body:undefined));;return buf.join("");
+buf.push("<!DOCTYPE html><html><head><title>" + (jade.escape(null == (jade_interp = title) ? "" : jade_interp)) + "</title></head><body><body>" + (jade.escape(null == (jade_interp = body) ? "" : jade_interp)) + "</body></body></html>");}.call(this,"title" in locals_for_with?locals_for_with.title:typeof title!=="undefined"?title:undefined,"body" in locals_for_with?locals_for_with.body:typeof body!=="undefined"?body:undefined));;return buf.join("");
 }});
