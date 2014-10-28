c1c8b1a86ae0995639534de0f365868fa08cbab6
Casey Foster <c@sey.me>
2014-10-28 14:18:41 -0500

    Remove magic JSX directive

--- test/env-1/jsx/a.jsx
+++ test/env-1/jsx/a.jsx
@@ -1,5 +1,3 @@
-/** @jsx React.DOM */
-
 var HelloMessage = React.createClass({
   render: function () {
     return <div>{'Hello ' + this.props.name}</div>;
--- test/env-1/jsx/expected.js
+++ test/env-1/jsx/expected.js
@@ -1,6 +1,4 @@
 // test/env-1/jsx/a.jsx
-/** @jsx React.DOM */
-
 var HelloMessage = React.createClass({displayName: 'HelloMessage',
   render: function () {
     return React.createElement("div", null, 'Hello ' + this.props.name);
