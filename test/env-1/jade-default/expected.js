c3fd337d8f5c56df43436468a34c71eade82540e
Casey Foster <c@sey.me>
2014-12-10 10:09:32 -0600

    Update tests

--- test/env-1/jade-default/expected.js
+++ test/env-1/jade-default/expected.js
@@ -12,6 +12,6 @@ return function template(locals) {
 var buf = [];
 var jade_mixins = {};
 var jade_interp;
-;var locals_for_with = (locals || {});(function (title, body) {
-buf.push("<!DOCTYPE html><html><head><title>" + (jade.escape(null == (jade_interp = title) ? "" : jade_interp)) + "</title></head><body><body>" + (jade.escape(null == (jade_interp = body) ? "" : jade_interp)) + "</body></body></html>");}.call(this,"title" in locals_for_with?locals_for_with.title:typeof title!=="undefined"?title:undefined,"body" in locals_for_with?locals_for_with.body:typeof body!=="undefined"?body:undefined));;return buf.join("");
+;var locals_for_with = (locals || {});(function (body, title) {
+buf.push("<!DOCTYPE html><html><head><title>" + (jade.escape(null == (jade_interp = title) ? "" : jade_interp)) + "</title></head><body><body>" + (jade.escape(null == (jade_interp = body) ? "" : jade_interp)) + "</body></body></html>");}.call(this,"body" in locals_for_with?locals_for_with.body:typeof body!=="undefined"?body:undefined,"title" in locals_for_with?locals_for_with.title:typeof title!=="undefined"?title:undefined));;return buf.join("");
 }});
