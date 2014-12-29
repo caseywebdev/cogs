4ec1aabb58d93610ff0ce3e6cc6b03b680da450d
Casey Foster <c@sey.me>
2014-12-28 21:49:19 -0600

    Fix broken tests

--- test/env-1/jsx/expected.js
+++ test/env-1/jsx/expected.js
@@ -1,5 +1,5 @@
 // test/env-1/jsx/a.jsx
-var HelloMessage = React.createClass({displayName: 'HelloMessage',
+var HelloMessage = React.createClass({displayName: "HelloMessage",
   render: function () {
     return React.createElement("div", null, 'Hello ' + this.props.name);
   }
