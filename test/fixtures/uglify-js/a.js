db72e91075626edad0894a558c52222e1a8e4362
Casey Foster <c@sey.me>
2015-01-21 10:15:01 -0600

    Add uglify-js and csso

---
+++ test/fixtures/uglify-js/a.js
@@ -0,0 +1,7 @@
+//= require ./b.js
+
+var a = 1;
+
+if (true === true) {
+  a = 2;
+}
---
+++ test/fixtures/uglify-js/expected.js
@@ -0,0 +1,4 @@
+// test/fixtures/uglify-js/b.js
+// I should be ignored by uglify-js
+// test/fixtures/uglify-js/a.js
+var a=1;a=2;
