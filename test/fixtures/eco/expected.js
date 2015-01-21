01b3745cd4c3a48861d8cc5342737c9a9ff6fda8
Casey Foster <c@sey.me>
2015-01-20 21:43:25 -0600

    Update 6to5

--- test/fixtures/eco/expected.js
+++ test/fixtures/eco/expected.js
@@ -11,7 +11,8 @@
     if (!__obj) __obj = {};
     var __out = [],
         __capture = function (callback) {
-      var out = __out, result;
+      var out = __out,
+          result;
       __out = [];
       callback.call(this);
       result = __out.join("");
