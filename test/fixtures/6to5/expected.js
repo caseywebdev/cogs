d1edc9acced93e4ca423ab83bc465009032c15ca
Casey Foster <c@sey.me>
2015-01-20 18:03:12 -0600

    Get integration tests running

--- test/fixtures/6to5/expected.js
+++ test/fixtures/6to5/expected.js
@@ -1,4 +1,4 @@
-// test/env-1/es6/a.es6
+// test/fixtures/6to5/a.es6
 (function (factory) {
   if (typeof define === "function" && define.amd) {
     define(["exports", "module", "jquery", "react"], factory);
@@ -7,17 +7,24 @@
   }
 })(function (exports, module, _jquery, _react) {
   "use strict";
+
   var _interopRequire = function (obj) {
     return obj && (obj["default"] || obj);
   };
+
   var $ = _interopRequire(_jquery);
+
   var React = _interopRequire(_react);
+
   var app = {
     init: function init() {
       $("body");
     }
   };
+
   $(app.init);
+
   React.createElement("div", null);
+
   module.exports = app;
 });
---
+++ test/lib/get-build.js
@@ -0,0 +1,30 @@
+var _ = require('underscore');
+var expect = require('chai').expect;
+var glob = require('glob');
+var getBuild = require('../../lib/get-build');
+var path = require('path');
+var fs = require('fs');
+
+var describe = global.describe;
+var it = global.it;
+
+describe('Integration Tests', function () {
+  var configPaths = glob.sync('test/fixtures/*/config.json');
+
+  _.each(configPaths, function (configPath) {
+    var name = configPath.match(/test\/fixtures\/(.*?)\/config.json/)[1];
+    var dir = path.dirname(configPath);
+    var config = require(path.resolve(configPath));
+    var fileName = glob.sync(path.join(dir, 'a.*'))[0];
+    var expected = fs.readFileSync(glob.sync(path.join(dir, 'expected.*'))[0]);
+
+    it(name, function (done) {
+      getBuild(fileName, config, function (er, build) {
+        if (er) done(er);
+        expect(build.buffer.toString().trim())
+          .to.equal(expected.toString().trim());
+        done();
+      });
+    });
+  });
+});
