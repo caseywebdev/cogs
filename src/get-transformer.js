6358ab8d8c1464de8dbcda7c65c90fbe044c010e
Casey Foster <c@sey.me>
2015-02-06 11:53:56 -0600

    Break down possible transformer errors for debugability

--- src/get-transformer.js
+++ src/get-transformer.js
@@ -5,6 +5,7 @@ module.exports = function (transformer) {
   transformer = _.clone(transformer);
   if (_.isString(transformer)) transformer = {name: transformer};
   if (!transformer.options) transformer.options = {};
+
   var name = transformer.name;
   var packageName = 'cogs-transformer-' + name;
   var requirePath;
@@ -15,19 +16,32 @@ module.exports = function (transformer) {
       '  Did you forget to `npm install ' + packageName + '`?'
     );
   }}
+
   try { transformer.fn = require(requirePath); } catch (er) {
     throw _.extend(er, {
       message: "Failed to load '" + name + "'\n  " + er.message
     });
   }
+
+  var package;
+  try { package = require(path.dirname(requirePath) + '/package'); }
+  catch (er) {
+  try { package = require(packageName + '/package'); } catch (er) {
+    throw new Error(
+      "Cannot find package file for '" + name + "'\n" +
+      '  Transformers are required to have a package file'
+    );
+  }}
+
   try {
-    transformer.version = require(packageName + '/package').version;
+    transformer.version = package.version;
     if (!transformer.version) throw new Error();
   } catch (er) {
     throw new Error(
-      "Cannot find package version for '" + name + "'\n" +
-      '  Transformers are required to specifiy a version in their package file'
+      "Cannot find version in package file for '" + name + "'\n" +
+      '  Transformers are required to specify a version in their package file'
     );
   }
+
   return transformer;
 };
