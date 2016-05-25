8ab70d8491528ecd61e3772d36eb65a28937c463
Casey Foster <c@sey.me>
2016-05-25 16:56:14 -0500

    Fix nglob ref, 2.0.9

--- src/get-glob-hash.js
+++ src/get-glob-hash.js
@@ -1,5 +1,5 @@
 var getHash = require('./get-hash');
-var glob = require('nglob');
+var glob = require('./nglob');
 
 module.exports = function (pattern, cb) {
   glob(pattern, {nodir: true}, function (er, files) {
