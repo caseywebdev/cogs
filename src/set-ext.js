2b560451766d39809c12a6a3d5485fcd5bdbe322
Casey Foster <c@sey.me>
2016-10-18 23:22:13 -0500

    More bugfixes, 3.0.6

--- src/set-ext.js
+++ src/set-ext.js
@@ -4,6 +4,6 @@ module.exports = (path, ext) => {
   if (ext == null) return path;
 
   const prev = npath.extname(path);
-  if (prev) path = npath.slice(0, -prev.length);
+  if (prev) path = path.slice(0, -prev.length);
   return path + ext;
 };
