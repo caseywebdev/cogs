72b87b465db562b3f5c84a664cb615cfc4babf2c
Casey Foster <c@sey.me>
2016-10-19 00:36:06 -0500

    Force consistent resolution order, 3.1.0

--- src/walk.js
+++ src/walk.js
@@ -2,19 +2,30 @@ const _ = require('underscore');
 const getFile = require('./get-file');
 const Promise = require('better-promise').default;
 
-const walk = ({env, path, visited = {}}) => {
-  if (visited[path]) return visited[path].then(file => [file]);
+const getFiles = ({env, files = {}, path}) => {
+  if (files[path]) return files;
 
-  return (visited[path] = getFile({env, path}))
-    .then(({requires}) =>
-      Promise.all(_.map(requires, path => walk({env, path, visited})))
-    )
-    .then(files => _.unique(_.flatten(files)))
-    .catch(er => {
-      const line = `\n  ${path}`;
-      if (er.message.indexOf(line) === -1) er.message += line;
-      throw er;
-    });
+  files[path] = true;
+  return getFile({env, path}).then(file => {
+    files[path] = file;
+    return Promise.all(_.map(file.requires, path =>
+      getFiles({env, files, path})
+    ));
+  }).then(() => files).catch(er => {
+    const line = `\n  ${path}`;
+    if (er.message.indexOf(line) === -1) er.message += line;
+    throw er;
+  });
 };
 
-module.exports = walk;
+const walk = ({files, path, visited = {}}) => {
+  const file = files[path];
+  if (visited[path]) return file;
+
+  visited[path] = true;
+  const graph = _.map(file.requires, path => walk({files, path, visited}));
+  return _.unique(_.flatten(graph));
+};
+
+module.exports = ({env, path}) =>
+  getFiles({env, path}).then(files => walk({files, path}));
