9b6fd15b766d519c0e1b87d867fb1796e380b0ed
Casey Foster <c@sey.me>
2017-05-09 11:20:15 -0500

    Simplify get-or-set

--- src/get-or-set.js
+++ src/get-or-set.js
@@ -1,6 +1,7 @@
 const Promise = require('better-promise').default;
 
 module.exports = (cache, key, fn) =>
-  Promise.isPromise(cache[key]) ? cache[key] :
-  cache[key] ? Promise.resolve(cache[key]) :
-  cache[key] = Promise.resolve().then(() => fn().then(val => cache[key] = val));
+  cache[key] || Promise.resolve().then(() => cache[key] = fn()).catch(er => {
+    delete cache[key];
+    throw er;
+  });
