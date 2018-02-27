771b0c10794c2d64166775cf7baf02543ab50480
Casey Foster <c@sey.me>
2018-02-27 09:21:06 -0600

    Use available space for formatSize, 4.2.6

--- src/format-size.js
+++ src/format-size.js
@@ -1,10 +1,9 @@
-const UNITS = ['B', 'K', 'M', 'G', 'T'];
+const UNITS = ['B', 'K', 'M', 'G', 'T', 'P', 'E'];
 
-const BASE = 1000;
+const {floor, log, min, pow, round} = Math;
 
 module.exports = size => {
-  const {floor, log, min, pow} = Math;
-  const i = min(UNITS.length - 1, floor(log(size) / log(BASE)));
-  const s = (size / pow(BASE, i)).toFixed(i && 1);
+  const i = min(UNITS.length - 1, floor(log(size) / log(1000)));
+  const s = (round((size / pow(1000, i)) * 1000) / 1000).toString().slice(0, 5);
   return (new Array(6 - s.length)).join(' ') + s + UNITS[i];
 };
