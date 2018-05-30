2eafbd9b515f7f366170cd9b64513ae8f8486aef
Casey Foster <c@sey.me>
2018-05-29 19:49:23 -0500

    Avoid log(0) error, 4.2.9

--- src/format-size.js
+++ src/format-size.js
@@ -3,7 +3,7 @@ const UNITS = ['B', 'K', 'M', 'G', 'T', 'P', 'E'];
 const {floor, log, min, pow, round} = Math;
 
 module.exports = size => {
-  const i = min(UNITS.length - 1, floor(log(size) / log(1000)));
+  const i = size ? min(UNITS.length - 1, floor(log(size) / log(1000))) : 0;
   const s = (round((size / pow(1000, i)) * 1000) / 1000).toString().slice(0, 5);
   return (new Array(6 - s.length)).join(' ') + s + UNITS[i];
 };
