784ac6c0229cf0d28f42a935270a511f557a8dac
Casey Foster <c@sey.me>
2018-06-08 15:54:39 -0500

    Fix bustCache, 4.4.1

--- src/bust-cache.js
+++ src/bust-cache.js
@@ -1,19 +1,12 @@
 const _ = require('underscore');
 const fileHasDependency = require('./file-has-dependency');
 
-const bustCache = async ({config: {envs, then}, path}) => {
-  await Promise.all(_.map(envs, async ({cache, then}) => {
+module.exports = async ({config, path}) =>
+  await Promise.all(_.map(config, async ({cache}) => {
     const {buffers, files} = cache;
     delete buffers[path];
     delete files[path];
     await Promise.all(_.map(files, async (file, key) => {
       if (fileHasDependency({file: await file, path})) delete files[key];
     }));
-
-    if (then) await bustCache({config: then, path});
   }));
-
-  if (then) await bustCache({config: then, path});
-};
-
-module.exports = bustCache;
