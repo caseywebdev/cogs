2a1a8810f31544b15b90d07260405bf201d27853
Casey Foster <c@sey.me>
2019-04-30 20:45:16 -0500

    Add space for 1023.9K, 4.5.3

--- src/format-size.js
+++ src/format-size.js
@@ -6,6 +6,6 @@ const BASE = 1024;
 
 module.exports = size => {
   const i = size ? min(UNITS.length - 1, floor(log(size) / log(BASE))) : 0;
-  const s = (round((size / BASE ** i) * BASE) / BASE).toString().slice(0, 5);
-  return new Array(6 - s.length).join(' ') + s + UNITS[i];
+  const s = (round((size / BASE ** i) * BASE) / BASE).toString().slice(0, 6);
+  return new Array(7 - s.length).join(' ') + s + UNITS[i];
 };
