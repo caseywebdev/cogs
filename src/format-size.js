bc7d16c3f94d49b06ec4bceb33020c6be2a342e0
Casey Foster <c@sey.me>
2019-04-30 21:19:58 -0500

    Adjust formatSize again, 4.5.5

--- src/format-size.js
+++ src/format-size.js
@@ -2,15 +2,10 @@ const UNITS = ['B', 'K', 'M', 'G', 'T', 'P', 'E'];
 
 const { floor, log, min, round } = Math;
 
+const BASE = 1024;
+
 module.exports = size => {
-  let i = size ? min(UNITS.length - 1, floor(log(size) / log(1024))) : 0;
-  let s = round(size / 1024 ** i).toString();
-
-  // Round 1000B - 1023B to 1K to keep it <= 4 characters.
-  if (s.length > 3) {
-    s = '1';
-    i += 1;
-  }
-
-  return new Array(4 - s.length).join(' ') + s + UNITS[i];
+  const i = size ? min(UNITS.length - 1, floor(log(size) / log(BASE))) : 0;
+  const s = (round((size / BASE ** i) * BASE) / BASE).toPrecision(4);
+  return new Array(6 - s.length).join(' ') + s + UNITS[i];
 };
