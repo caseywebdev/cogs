a49da00bb9b0434aadd4fef9d5569e5597609801
Casey Foster <c@sey.me>
2019-04-30 20:22:43 -0500

    Fix formatSize, 4.5.2

--- src/format-size.js
+++ src/format-size.js
@@ -2,8 +2,10 @@ const UNITS = ['B', 'K', 'M', 'G', 'T', 'P', 'E'];
 
 const { floor, log, min, round } = Math;
 
+const BASE = 1024;
+
 module.exports = size => {
-  const i = size ? min(UNITS.length - 1, floor(log(size) / log(1000))) : 0;
-  const s = (round((size / 1000 ** i) * 1000) / 1000).toString().slice(0, 5);
+  const i = size ? min(UNITS.length - 1, floor(log(size) / log(BASE))) : 0;
+  const s = (round((size / BASE ** i) * BASE) / BASE).toString().slice(0, 5);
   return new Array(6 - s.length).join(' ') + s + UNITS[i];
 };
