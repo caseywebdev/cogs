4d9d217d911554854f9dd198cd404fb2052fc6fb
Casey Foster <c@sey.me>
2019-04-30 21:03:46 -0500

    Use shorter format, 4.5.4

--- src/format-size.js
+++ src/format-size.js
@@ -2,10 +2,15 @@ const UNITS = ['B', 'K', 'M', 'G', 'T', 'P', 'E'];
 
 const { floor, log, min, round } = Math;
 
-const BASE = 1024;
-
 module.exports = size => {
-  const i = size ? min(UNITS.length - 1, floor(log(size) / log(BASE))) : 0;
-  const s = (round((size / BASE ** i) * BASE) / BASE).toString().slice(0, 6);
-  return new Array(7 - s.length).join(' ') + s + UNITS[i];
+  let i = size ? min(UNITS.length - 1, floor(log(size) / log(1024))) : 0;
+  let s = round(size / 1024 ** i).toString();
+
+  // Round 1000B - 1023B to 1K to keep it <= 4 characters.
+  if (s.length > 3) {
+    s = '1';
+    i += 1;
+  }
+
+  return new Array(4 - s.length).join(' ') + s + UNITS[i];
 };
